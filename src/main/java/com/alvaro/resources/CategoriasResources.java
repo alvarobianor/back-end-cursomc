a3ad1ced25c9b6b9fdfe7093455ad6f5294abf15
Álvaro Bianor <bianor.alvaro12@gmail.com>
2019-06-09 16:31:07 -0300

    Atualizando categoriaREST

--- src/main/java/com/alvaro/resources/CategoriasResources.java
+++ src/main/java/com/alvaro/resources/CategoriasResources.java
@@ -1,15 +1,27 @@
 package com.alvaro.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alvaro.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriasResources {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Dando certo REST Categorias";
+	public List<Categoria> listar() {
+		Categoria categoria = new Categoria(1, "Sexshop");
+		Categoria categoria2 = new Categoria(2, "Brinquedos");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(categoria);
+		lista.add(categoria2);
+		
+		return lista;
 	}
 }
